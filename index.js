18cd300a9b99d12081d220e44fa96535e6b1af2b
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-10 04:14:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const PwEl = document.getElementById("pw");
 const copyEl = document.getElementById("copy");
 const lenEl = document.getElementById("len");
 const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
