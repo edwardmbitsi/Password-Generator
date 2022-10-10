12fa0e20c19943ddfba47b686dcf3d26b06a6538
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-10 04:19:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const upperEl = document.getElementById("upper");
 const lowerEl = document.getElementById("lower");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementId("generate");
+const numberEl = document.getElementById("number");
